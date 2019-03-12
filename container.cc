78b4f061e7cbabf896ec1145c644b22c1502b76a
Begori <bulgakov_egor@outlook.com>
2019-03-12 16:40:53 +0300

    Stack memory function and clone()

--- container.cc
+++ container.cc
@@ -1,8 +1,31 @@
-#include <stdio.h>
+#include <iostream>
+#include <sys/wait.h>
 
-int main(int argc, char* argv[])
+char* stack_memory()
 {
-	printf("Hello\n");
+	int stackSize = 65536;
+	char* stack = new (std::nothrow) char[stackSize];
+
+	if (stack == nullptr)
+	{
+		printf("Cannot allocate memory \n");
+		exit(1);
+	}
+
+	return stack + stackSize;
+}
+
+int jail(void* args)
+{
+	printf("Hello from child\n");
+
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	printf("Hello from parent\n");
+	clone(jail, stack_memory(), SIGCHLD, 0);
 
 	return 0;
 }
